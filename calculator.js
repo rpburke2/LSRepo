2a887bd104cc527aebf68f8ea7d0b9df200085ad
Robert Burke <34951191+rpburke2@users.noreply.github.com>
2021-07-22 10:32:25 -0400

    origin https://github.com/rpburke2/LSRepo

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
-// Ask the user for the first number.
-// Ask the user for the second number.
-// Ask the user for an operation to perform.
-// Perform the operation on the two numbers.
-// Print the result to the terminal.
+// Ask the user for the first number
+// Ask the user for the second number
+// Ask the user for an operation to perform
+// Perform the operation on the two numbers
+// Print the result to the terminal
 
 const readline = require('readline-sync');
 
