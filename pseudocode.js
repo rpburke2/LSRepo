ea663613cef856ff4f3c13316a76d66e7ece2320
Robert Burke <34951191+rpburke2@users.noreply.github.com>
2021-07-22 11:56:05 -0400

    additional info

---
+++ pseudocode.js
@@ -0,0 +1,76 @@
+/* 
+
+/// Function that returns the sum of two numbers ///
+
+START 
+
+GET number1 = user input
+GET number 2 = user input
+
+SET sumOfNums = number1 + number2
+
+PRINT sumOfNums
+
+END
+
+
+
+
+/// Function that takes an array of strings, and returns a string ///
+          that is all those strings concatenated together
+
+
+START
+
+SET empty array
+
+GET first string user inputs
+GET second string user inputs
+GET third string user inputs
+
+WHILE string <= length of stringArray
+  SET string = 0
+  
+
+
+
+END
+
+
+
+
+/// Function that takes an array of integers, and ///
+    returns a new array with every other element
+
+START
+
+
+
+END
+
+
+
+
+START
+
+# Given a collection of integers called "numbers"
+
+SET iterator = 1
+SET savedNumber = value within numbers collection at space 1
+
+WHILE iterator <= length of numbers
+  SET currentNumber = value within numbers collection at space "iterator"
+  IF currentNumber > savedNumber
+    savedNumber = currentNumber
+  ELSE
+    skip to the next iteration
+
+  iterator = iterator + 1
+
+PRINT savedNumber
+
+END
+
+
+
+*/
