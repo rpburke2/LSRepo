740c3169fef9c8cadb790f3860cf9526e7e6d54f
Robert Burke <34951191+rpburke2@users.noreply.github.com>
2021-07-28 15:44:39 -0400

    car payment calc

---
+++ car_payment_calculator.js
@@ -0,0 +1,20 @@
+/* car payment calculator
+
+  - Ask user for Loan Amount
+  - Ask user for the Annual Percentage Rate (APR)
+  - Ask user for the Loan Duration
+  - Calculate Monthly Interest Rate
+  - Loan Duration in Months
+
+  m = Monthly payment
+  p = Loan amount
+  j = Monthly interest rate
+  n = Loan during in months
+
+
+*/
+
+
+
+const readline = require('readline-sync');
+
