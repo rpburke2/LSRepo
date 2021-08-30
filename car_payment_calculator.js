4828c14926f8bca7e4e03a9f997258a8a3479e3b
Robert Burke <34951191+rpburke2@users.noreply.github.com>
2021-08-30 13:21:10 -0400

    Added car loan calculator function console logs

--- car_payment_calculator.js
+++ car_payment_calculator.js
@@ -3,13 +3,10 @@
   - Ask user for Loan Amount
   - Ask user for the Annual Percentage Rate (APR)
   - Ask user for the Loan Duration
-  - Calculate Monthly Interest Rate
-  - Loan Duration in Months
 
-  - Monthly payment - m - 
-  - Loan amount - p - loanDetials.amount
-  - Monthly interest rate - j - loanDetails.apr
-  - Loan duration in months - n - loanDetials.months
+  - Calculate Monthly Interest Rate for user
+  - Give user Loan Duration in Months
+  - Give user Monthly Interest Rate
 
 
 */
@@ -34,17 +31,16 @@ function isValidNumber(number) {
 }
 
 
-prompt('--------------------------------');
+prompt('');
+prompt(`***** ${config.greeting} ******`);
+prompt('');
 
-prompt(config.greeting);
-
-prompt('--------------------------------');
 
 function getLoanAmount() {
   prompt(config.loanDetails.amount); 
   let loanAmount = readline.question();
 
-  while (loanAmount < 0 || Number.isNaN(Number(loanAmount)) || loanAmount.trimStart() === '') {
+  while (isValidNumber(loanAmount)) {
     prompt("It looks like you've entered an invalid loan amount! ");
     loanAmount = readline.question();
   }
@@ -56,8 +52,8 @@ function getLoanLength() {
   prompt(config.loanDetails.months);
   let loanMonths = readline.question();
 
-  while (loanMonths < 0 || Number.isNaN(Number(loanMonths)) || loanMonths.trimStart() === '' || (((loanMonths * 12) % 12) !== 0)); {
-    prompt("It looks like you've entered an invalid amount of years! ");
+  while (isValidNumber(loanMonths)) {
+    prompt("It looks like you've entered an invalid amount of months! ");
     loanMonths = readline.question();
   }
 
@@ -68,13 +64,22 @@ function getAPR() {
   prompt(config.loanDetails.apr);
   let loanAPR = readline.question();
 
-  while (loanAPR > 100 || loanAPR < 0 || Number.isNaN(Number(loanAPR)) || loanAPR.trimStart() === '') {
+  while (isValidNumber(loanAPR)) {
     prompt("It looks like you've entered an invalid APR! ");
     loanAPR = readline.question();
   }
 
-  return Number(loanAPR) / 12 / 200;
+  return Number(loanAPR) / 12 / 100;
 }
+
+function calculatePayment(amount, months, interest) {
+  if (amount === 0) return 0;
+  else if (interest === 0) return amount / months;
+  else if (months === 0) return amount;
+
+  return amount * (interest / (1 - Math.pow((1 + interest), (-months))));
+}
+
 
 let loanAmount = getLoanAmount();
 
@@ -82,10 +87,26 @@ let loanMonths = getLoanLength();
 
 let loanAPR = getAPR();
 
-let monthlyPayment = loanAmount * (loanAPR / (1 - Math.pow((1 + loanAPR), (-loanMonths))));
+let monthlyPayment = calculatePayment(loanAmount, loanMonths, loanAPR);
 
 
-console.log(config.loanCalculated.payment.replace('<AMOUNT>', monthlyPayment));
+prompt('----------');
+prompt('');
 
+prompt(config.loanCalculated.payment.replace('<AMOUNT>', monthlyPayment.toFixed(2)));
 
-console.log('Thanks for using our Payment Calculator!!');
+prompt('--');
+
+prompt(config.loanCalculated.months.replace('<AMOUNT>', loanMonths));
+
+prompt('--');
+
+prompt(config.loanCalculated.apr.replace('<AMOUNT>', loanAPR * 12 * 100));
+
+prompt('');
+prompt('----------');
+prompt('');
+
+prompt('Thanks for using our Payment Calculator! See you next time :)');
+
+prompt('');
