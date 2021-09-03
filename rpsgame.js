6d80331fc4afbd0af85732b4aeb7e9ac8828ec4e
Robert Burke <34951191+rpburke2@users.noreply.github.com>
2021-09-03 11:05:16 -0400

    Play again loop added

--- rpsgame.js
+++ rpsgame.js
@@ -26,29 +26,37 @@ function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
-prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-let choice = readline.question();
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question();
 
-while (!VALID_CHOICES.includes(choice)) {
-  prompt("That's not a valid choice.");
-  choice = readline.question();
-}
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice.");
+    choice = readline.question();
+  }
 
-let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-let computerChoice = VALID_CHOICES[randomIndex];
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
 
-prompt(`You chose ${choice}. Computer chose ${computerChoice}.`);
+  prompt(`You chose ${choice}. Computer chose ${computerChoice}.`);
 
-
-if ((choice === 'rock' && computerChoice === 'scissors') ||
+  if ((choice === 'rock' && computerChoice === 'scissors') ||
     (choice === 'scissors' && computerChoice === 'paper') ||
     (choice === 'paper' && computerChoice === 'rock')) {
-  prompt('You win!');
-} else if ((choice === 'scissors' && computerChoice === 'rock') ||
+    prompt('You win!');
+  } else if ((choice === 'scissors' && computerChoice === 'rock') ||
     (choice === 'paper' && computerChoice === 'scissors') ||
     (choice === 'rock' && computerChoice === 'paper')) {
-  prompt('Computer won!');
-} else {
-  prompt("It's a tie!");
-}
+    prompt('Computer won!');
+  } else {
+    prompt("It's a tie!");
+  }
+  prompt('Would you like to play again? (y/n)');
+  let answer = readline.question().toLowerCase();
 
+  while (answer[0] !== 'y' && answer[0] !== 'n') {
+    prompt("Please enter 'y' or 'n'");
+    answer = readline.question();
+  }
+  if (answer[0] !== 'y') break;
+}
