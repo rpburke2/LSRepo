0c84e05f9e1a62f44be91a7f4f8a09e3e14feee1
Robert Burke <34951191+rpburke2@users.noreply.github.com>
2021-09-10 18:34:36 -0400

    Practice Problems | Easy

---
+++ Lesson_3/practice_problems.js
@@ -0,0 +1,17 @@
+let str1 = 'Come over here!';
+let str2 = "What's up, Doc?";
+
+console.log(str1.endsWith('!'));
+
+console.log(str2.endsWith('!'));
+
+
+let ages = { 
+  Herman: 32, 
+  Lily: 30, 
+  Grandpa: 402, 
+  Eddie: 10
+};
+
+console.log(ages.hasOwnProperty('Spot'));
+
