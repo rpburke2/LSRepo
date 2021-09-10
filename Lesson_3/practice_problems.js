5a26f8d03a188392d51abad87a75f4550193d59c
Robert Burke <34951191+rpburke2@users.noreply.github.com>
2021-09-10 18:56:06 -0400

    More practice questions | easy

--- Lesson_3/practice_problems.js
+++ Lesson_3/practice_problems.js
@@ -15,3 +15,9 @@ let ages = {
 
 console.log(ages.hasOwnProperty('Spot'));
 
+
+let munstersDescription = "the Munsters are CREEPY and Spooky.";
+// => The munsters are creepy and spooky.
+
+console.log(munstersDescription.charAt(0).toUpperCase() + munstersDescription.substring(1).toLowerCase());
+
