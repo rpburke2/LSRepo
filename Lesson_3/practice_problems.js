8905c68a5ac9d39e90dcba7b2cc50cf0e4dcc16f
Robert Burke <34951191+rpburke2@users.noreply.github.com>
2021-09-10 19:21:36 -0400

    Practice problems | easy - remaining questions complete

--- Lesson_3/practice_problems.js
+++ Lesson_3/practice_problems.js
@@ -1,9 +1,9 @@
-let str1 = 'Come over here!';
-let str2 = "What's up, Doc?";
+// let str1 = 'Come over here!';
+// let str2 = "What's up, Doc?";
 
-console.log(str1.endsWith('!'));
+// console.log(str1.endsWith('!'));
 
-console.log(str2.endsWith('!'));
+// console.log(str2.endsWith('!'));
 
 
 let ages = { 
@@ -13,11 +13,40 @@ let ages = {
   Eddie: 10
 };
 
-console.log(ages.hasOwnProperty('Spot'));
+let additionalAges = { 
+  Marilyn: 22, 
+  Spot: 237 
+};
+
+Object.assign(ages + additionalAges);
 
 
-let munstersDescription = "the Munsters are CREEPY and Spooky.";
-// => The munsters are creepy and spooky.
+// let munstersDescription = "the Munsters are CREEPY and Spooky.";
+// // => The munsters are creepy and spooky.
 
-console.log(munstersDescription.charAt(0).toUpperCase() + munstersDescription.substring(1).toLowerCase());
+// console.log(munstersDescription.charAt(0).toUpperCase() + munstersDescription.substring(1).toLowerCase());
 
+
+let str3 = "Few things in life are as important as house training your pet dinosaur.";
+let str4 = "Fred and Wilma have a pet dinosaur named Dino.";
+
+console.log(str3.includes('Dino'));
+console.log(str4.includes('Dino'));
+
+
+
+let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+
+// flintstones.push("Dino");
+
+flintstones.push("Dino", "Hoppy");
+
+
+let advice = "Few things in life are as important as house training your pet dinosaur.";
+
+// Expected return value:
+// => 'Few things in life are as important as '
+
+advice.slice(0, advice.indexOf('house'));
+
+console.log(advice);
