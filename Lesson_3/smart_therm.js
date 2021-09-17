0e99b503de07b66d4b445afa0343de7458fc2d46
Robert Burke <34951191+rpburke2@users.noreply.github.com>
2021-09-17 10:44:43 -0700

    smart home thermometer problem

---
+++ Lesson_3/smart_therm.js
@@ -0,0 +1,11 @@
+// Testing out 'strictmode'
+
+'use strict';
+
+// PROBLEM:
+/** smart home thermometer that needs sort through a given array of daily temps and 
+calculate the temperature amplitude. There also may be an error thrown. **/
+
+const temperatures = [20, 30, 15, 0, 'error', 45, 65, 73, 86, 95];
+
+// 
