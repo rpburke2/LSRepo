8f1d6bdb1909949a22e775692d46c3482dfd21db
Robert Burke <34951191+rpburke2@users.noreply.github.com>
2021-09-23 11:20:23 -0700

    calculate amplitude

--- Lesson_3/smart_therm.js
+++ Lesson_3/smart_therm.js
@@ -4,8 +4,25 @@
 
 // PROBLEM:
 /** smart home thermometer that needs sort through a given array of daily temps and 
-calculate the temperature amplitude. There also may be an error thrown. **/
+calculate the temperature amplitude. There also may be a sensor error thrown. **/
 
 const temperatures = [20, 30, 15, 0, 'error', 45, 65, 73, 86, 95];
 
-// 
+/** 1) Understand the problem:
+- What is temp amplitude? The difference between the highest and lowest temp
+- How to compute max and min temps?
+- What is a sensor error? What to do?
+
+2) Breaking down into sub-problems:
+- How to ignore errors?
+- Find max temp in array.
+- Find min temp in array.
+- Subtract min from max (amplitude) and return it.
+**/
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    
+  } 
+};
